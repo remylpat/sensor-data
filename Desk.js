36fb1168f79079278df189c498be561deffb3a5a
fabmarti17 <89384739+fabmarti17@users.noreply.github.com>
2023-02-03 14:05:42 +0100

    Update and rename metrics.js to Desk.js

--- Desk.js
+++ Desk.js
@@ -6,23 +6,18 @@ xapi.Config.HttpClient.AllowInsecureHTTPS.set('True');
 xapi.Config.HttpClient.Mode.set('On');
 xapi.Config.RoomAnalytics.AmbientNoiseEstimation.Mode.set('On');
 
-
-
 const baseUrl = 'https://fabmarti17.github.io/sensor-data/sensors.html';
 
 //Update frequency
 const updateSec = 300;
-
 
 async function updateUrl() {
   const temp = await xapi.Status.RoomAnalytics.AmbientTemperature.get();
   const hum = await xapi.Status.RoomAnalytics.RelativeHumidity.get();
   const anoise = await xapi.Status.RoomAnalytics.AmbientNoise.Level.A.get();
   const name = await xapi.Status.SystemUnit.BroadcastName.get();
-// AIR QUALITY 
-//only avialable if RoomNavigator
-//  const airquality = await xapi.Status.RoomAnalytics.airquality.get();
-const url = baseUrl + '?temp=' + temp + '&humidity=' + hum + '&ambientnoise=' +anoise + '&name=' + name ; // + '&airquality' + airquality;
+
+  const url = baseUrl + '?temp=' + temp + '&humidity=' + hum + '&ambientnoise=' +anoise + '&name=' + name ; // + '&airquality' + airquality;
   xapi.Config.Standby.Signage.Url.set(url);
   console.log('signage url updated:', url);
 }
