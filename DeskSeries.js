ac18284e1ed2300f4e80b1f3fc707e6cac5f84ec
fabmarti17 <89384739+fabmarti17@users.noreply.github.com>
2023-02-06 09:39:16 +0100

    Update DeskSeries.js

--- DeskSeries.js
+++ DeskSeries.js
@@ -12,12 +12,15 @@ const baseUrl = 'https://fabmarti17.github.io/sensor-data/deskseries.html';
 const updateSec = 300;
 
 async function updateUrl() {
-  const temp = await xapi.Status.RoomAnalytics.AmbientTemperature.get();
-  const hum = await xapi.Status.RoomAnalytics.RelativeHumidity.get();
+  const temp = await xapi.Status.Peripherals.ConnectedDevice.RoomAnalytics.AmbientTemperature.get();
+  const hum = await xapi.Status.Peripherals.ConnectedDevice.RoomAnalytics.RelativeHumidity.get();
   const anoise = await xapi.Status.RoomAnalytics.AmbientNoise.Level.A.get();
   const name = await xapi.Status.SystemUnit.BroadcastName.get();
+  const pccur = await xapi.Status.RoomAnalytics.PeopleCount.Current.get()
+  const pccap = await xapi.Status.RoomAnalytics.PeopleCount.Capacity.get()
+  const sound = await xapi.Status.RoomAnalytics.Sound.Level.A.get()
 
-  const url = baseUrl + '?temp=' + temp + '&humidity=' + hum + '&ambientnoise=' +anoise + '&name=' + name ; // + '&airquality' + airquality;
+  const url = baseUrl + '?temp=' + temp + '&humidity=' + hum + '&ambientnoise=' +anoise + '&name=' + name + '&peoplecount=' + pccur + '/' + pccap + '&sound=' + sound;
   xapi.Config.Standby.Signage.Url.set(url);
   console.log('signage url updated:', url);
 }
